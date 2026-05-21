3ba5c36528098a4252848dee60f6c470f014e442
ErnestasVal <valentinas.ernestas@gmail.com>
2026-05-21 20:31:11 +0300

    Cyclic reference fix

--- Assets/UI/ui.gd
+++ Assets/UI/ui.gd
@@ -2,7 +2,6 @@ class_name UI
 extends CanvasLayer
 @export var hudOn = true
 @export var mouse_mode_action : StringName = "play_char_mouse_mode_action"
-@export var main_menu_scene : PackedScene
 @onready var animation : AnimationPlayer = $AnimationPlayer
 @onready var hud : Control = $InGame
 @onready var pause : Container = $PanelContainer
@@ -13,11 +12,10 @@ func _ready() -> void:
 	if !hudOn:
 		hud.visible = false
 	animation.play("EnterLevel")
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed(mouse_mode_action):
+	if Input.is_action_just_pressed(mouse_mode_action) and hudOn:
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			pause.visible = true
 			get_tree().paused = true
@@ -37,20 +35,20 @@ func _on_resume_pressed() -> void:
 func _on_restart_pressed() -> void:
 	level_change()
 	
-func _do_change_level(level : PackedScene):
-	if level:
-		get_tree().change_scene_to_packed(level)
+func _do_change_level(go_to_main_menu: bool):
+	if go_to_main_menu:
+		get_tree().change_scene_to_file("res://Levels/main_menu.tscn")
 	else:
 		get_tree().reload_current_scene()
 
 
 func _on_quit_pressed() -> void:
-	level_change(main_menu_scene)
+	level_change(true)
 	
-func level_change(level : PackedScene = null):
+func level_change(go_to_main_menu: bool = false):
 	Input.action_press(mouse_mode_action)
 	Input.action_release(mouse_mode_action)
 	_level_change_tween = create_tween()
 	_level_change_tween.tween_interval(0.5)
-	_level_change_tween.tween_callback(_do_change_level.bind(level))
+	_level_change_tween.tween_callback(_do_change_level.bind(go_to_main_menu))
 	exitLevel()
